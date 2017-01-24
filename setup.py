75a8648acf734f6e2f294c001d489917e7582bac
Jason Grout <jgrout6@bloomberg.net>
2017-01-24 14:28:09 -0500

    0.4.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not os.path.exists('node_modules'):
 
 setup_args = dict(
     name                 = 'jupyterlab_geojson',
-    version              = '0.3.0',
+    version              = '0.4.0',
     packages             = ['jupyterlab_geojson'],
     author               = 'Jason Grout',
     author_email         = 'jgrout6@bloomberg.net',
